2bf2cdd03c7cbcf984bedf613f0c02d264e11c6e
Juan Rodriguez <juanpablo.rodriguezgomez@gmail.com>
2017-03-06 20:31:33 +0100

    changing accelerations

---
+++ DeriveJointAcceleration.m
@@ -0,0 +1,10 @@
+function [ q1dd, q2dd] = DeriveJointAcceleration( M,C,G,acr )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+    % From the formula obtained
+    q2dd = simplify((M(1,1)*(acr.T2-C(2)-G(2))+M(1,2)*(C(1)+G(1)))/(M(1,1)*M(2,2)-M(1,2)^2));
+    q1dd = simplify(-(M(1,2)*q2dd+C(1)+G(1))/(M(1,1)));
+
+end
+
